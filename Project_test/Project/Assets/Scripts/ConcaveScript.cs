40d6ea061f5c9ebb29067ccfad3805f821a0e916
Colm Curtis <colmcurtis@hotmail.com>
2017-03-06 01:18:13 +0000

    Another Commit
    Concave Mirror Experiment almost working

--- Project_test/Project/Assets/Scripts/ConcaveScript.cs
+++ Project_test/Project/Assets/Scripts/ConcaveScript.cs
@@ -3,37 +3,80 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ConcaveScript : MonoBehaviour {
+    public ResultsScript rs;
     public Material image;
-    public Text test;
-    private float a,r,g,b;
-    private Color orginal;
+    public Text test,uSliderText,vSliderText;
+    public Slider uSlider,vSlider;
+    public GameObject mirror,vBeam,uBeam,screen,screenDispaly;
+    public Toggle UI;
+    private float a,r,g,b,sqrtHalf,u,v,f,correctValue,difference;
+    private Vector3 vPos,screenPos, screenDisplayPos;
+    private float[] uResults,vResults;
+    private int count;
 	// Use this for initialization
 	void Start () {
-        //image.color = new Color(255, 255, 255, 255);
-        orginal = image.color;
-        a = image.color.a;
-        r = image.color.r;
-        g = image.color.g;
-        b = image.color.b;
+        image.color = new Color(1,1,1, 0);
+        sqrtHalf = Mathf.Sqrt(0.5f);
+        f = 20;
+        screenDisplayPos = Camera.main.ScreenToWorldPoint(new Vector3(100, 100, 200));
+        screenDispaly.transform.position = screenDisplayPos;
+        uResults = new float[10];
+        vResults = new float[10];
+        vPos = new Vector3(vBeam.transform.position.x, vBeam.transform.position.y, vBeam.transform.position.z);
+        screenPos = new Vector3(screen.transform.position.x, screen.transform.position.y, screen.transform.position.z);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.KeypadMinus))
+        if(vSlider.interactable == true)
         {
-            image.color = new Color(image.color.r, image.color.g, image.color.b, (image.color.a)+0.001f);
-            Debug.Log("Pressing -");
+            v = 40 + (vSlider.value * 2);
+            difference = Mathf.Abs(v - correctValue);
+            image.color = new Color(image.color.r, image.color.g, image.color.b, (255-difference*30)/255);
         }
-        if (Input.GetKey(KeyCode.KeypadPlus))
+        test.text = "u = " + u.ToString("f1")+ " v = " + v+ " real v = " + correctValue.ToString("f2")+ " a = " + image.color.a*255;
+    }
+    public void uChange()
+    {
+        mirror.transform.position = new Vector3(mirror.transform.position.x, mirror.transform.position.y, 75 - uSlider.value);
+        screen.transform.position = new Vector3(screen.transform.position.x, screen.transform.position.y, screenPos.z - uSlider.value);
+        vBeam.transform.position = new Vector3(vBeam.transform.position.x, vBeam.transform.position.y, vPos.z - uSlider.value);
+        uBeam.transform.position = new Vector3(uBeam.transform.position.x, uBeam.transform.position.y, 6 - (uSlider.value*0.5f));
+        uBeam.transform.localScale = new Vector3(uBeam.transform.localScale.x, 70 - (uSlider.value*0.5f), uBeam.transform.localScale.z);
+        uSliderText.text = (100 - uSlider.value).ToString("f1") + " cm";
+    }
+    public void setU()
+    {
+        vPos = new Vector3(vBeam.transform.position.x, vBeam.transform.position.y, vBeam.transform.position.z);
+        screenPos = new Vector3(screen.transform.position.x, screen.transform.position.y, screen.transform.position.z);
+        uSlider.interactable = false;
+        vSlider.interactable = true;
+        u = 100 - uSlider.value;
+        correctValue = (u * f) / (u - f);
+    }
+    public void recordResult()
+    {
+        uResults[count] = u;
+        vResults[count] = v;
+        count++;
+        Debug.Log("Result " + count + " recorded");
+        vSlider.interactable = false;
+        uSlider.interactable = true;
+        vPos = new Vector3(vBeam.transform.position.x, vBeam.transform.position.y, vBeam.transform.position.z);
+        screenPos = new Vector3(screen.transform.position.x, screen.transform.position.y, screen.transform.position.z);
+    }
+    public void vChange()
+    {
+        screen.transform.position = new Vector3(17.5f + (vSlider.value*1.4f), screenPos.y, screenPos.z - (vSlider.value*1.4f));
+        vBeam.transform.position = new Vector3(-0.1f+(vSlider.value*sqrtHalf), vPos.y, vPos.z - (vSlider.value*sqrtHalf));
+        vBeam.transform.localScale = new Vector3(vBeam.transform.localScale.x, 24+ (vSlider.value ), vBeam.transform.localScale.z);
+        vSliderText.text = (40 + (vSlider.value*2)).ToString("f1") + " cm";
+    }
+    public void OnGUI()
+    {
+        if (UI.isOn == true)
         {
-            image.color = new Color(image.color.r, image.color.g, image.color.b, (image.color.a) - 0.001f);
-            Debug.Log("Pressig +");
+            rs.TwoResults("Distance U (cm)", "Distance V (cm)", uResults, vResults, count);
         }
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            image.color = orginal;
-            Debug.Log("Reset");
-        }
-        test.text = "a = " + image.color.a+ " r = " + image.color.r+ " g = " + image.color.g+ " b = " + image.color.b;
     }
 }
