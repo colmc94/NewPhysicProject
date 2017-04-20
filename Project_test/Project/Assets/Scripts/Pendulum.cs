76562c07711679448780cec4708144c5af2a7bc6
colmc94 <colmcurtis@hotmail.com>
2017-04-20 16:15:19 +0100

    Laptop Commit
    Pendulum updated on laptop

--- Project_test/Project/Assets/Scripts/Pendulum.cs
+++ Project_test/Project/Assets/Scripts/Pendulum.cs
@@ -6,17 +6,18 @@ public class Pendulum : MonoBehaviour {
     public GameObject ball,cork;
     public Slider ballPositionSlider,ballHeightSlider;
     public Material white;
-    public Text TimeText;
-    private Rigidbody rbBall,rbCork;
-    private float distance,temp,ypos,z,speed,elapsedTime,finishTime;
-    private Renderer normalRend;
+    public Text TimeText,heightText;
+    public ResultsScript resultScript;
+    public Toggle UI;
+    private float[] time1Results, time2Results, heightResults, gResults;
+    private Rigidbody rbBall;
+    private float distance,temp,ypos,z,speed,elapsedTime,finishTime,w,h;
     private LineRenderer lr1;
-    private int count;
+    private int count,i;
     private HingeJoint hinge;
     // Use this for initialization
     void Start () {
         rbBall = GetComponent<Rigidbody>();
-        rbCork = cork.GetComponent<Rigidbody>();
         lr1 = cork.AddComponent<LineRenderer>();
         lr1.material = white;
         //lr1.SetColors(Color.yellow, Color.yellow);
@@ -24,29 +25,48 @@ public class Pendulum : MonoBehaviour {
         distance = Vector3.Distance(ball.transform.position, cork.transform.position);
         hinge = cork.AddComponent<HingeJoint>();
         hinge.connectedBody = rbBall;
+        time1Results = new float[10];
+        time2Results = new float[10];
+        heightResults = new float[10];
+        gResults = new float[10];
         Debug.Log("Distance: " + distance);
-	}
+        w = 1920f;
+        h = 1080;
+    }
 	// Update is called once per frame
 	void FixedUpdate () {
         if (rbBall.useGravity)
         {
-            if (count >= 60)
+            if (count == 60)
             {
-                TimeText.text = elapsedTime.ToString("f4");
+                TimeText.text = elapsedTime.ToString("f2");
+                storeResults();
             }
             else
             {
                 elapsedTime = elapsedTime + Time.fixedDeltaTime;
-                TimeText.text = (elapsedTime).ToString("f4");
+                TimeText.text = (elapsedTime).ToString("f2");
             }
         }
-        Debug.Log("Distance: " + distance + " y : "+ypos+" count = "+count/2);
+        distance = Vector3.Distance(ball.transform.position, cork.transform.position);
         lr1.SetPosition(0, cork.transform.position);
         lr1.SetPosition(1, ball.transform.position);
     }
-    void OnTriggerEnter(Collider other)
+    void OnTriggerExit(Collider other)
     {
         count++;
+        Debug.Log("Count = " + count / 2 + " oscillations");
+    }
+    private void storeResults()
+    {
+        time1Results[i] = elapsedTime;
+        time2Results[i] = elapsedTime / 30;
+        heightResults[i] = distance/100;
+        float g = ((4 * (Mathf.PI* Mathf.PI)) * (distance/100)) / ((elapsedTime / 30)* (elapsedTime / 30));
+        gResults[i] = g;
+        i++;
+        Debug.Log("Result " + i + " stored. G= "+g+ "m/s^2");
+        ResetExp();
     }
     public void StartExp()
     {
@@ -67,6 +87,8 @@ public class Pendulum : MonoBehaviour {
         z = temp - cork.transform.position.z;
         ypos = Mathf.Sqrt((distance * distance)-(z*z))-cork.transform.position.y;
         ball.transform.position = new Vector3(0,Mathf.Abs(ypos), 2.5f+ballPositionSlider.value);
+        lr1.SetPosition(0, cork.transform.position);
+        lr1.SetPosition(1, ball.transform.position);
         distance = Vector3.Distance(ball.transform.position, cork.transform.position);
     }
     public void setHeight()
@@ -74,8 +96,21 @@ public class Pendulum : MonoBehaviour {
         Destroy(hinge);
         ballPositionSlider.value = 0;
         ball.transform.position = new Vector3(0, ballHeightSlider.value, 2.5f + ballPositionSlider.value);
+        lr1.SetPosition(0, cork.transform.position);
+        lr1.SetPosition(1, ball.transform.position);
         distance = Vector3.Distance(ball.transform.position, cork.transform.position);
         hinge = cork.AddComponent<HingeJoint>();
         hinge.connectedBody =rbBall;
+        heightText.text = distance.ToString("f2") + " m";
+    }
+    public void OnGUI()
+    {
+        float ratioX = Screen.width / w;
+        float ratioY = Screen.height / h;
+        GUI.matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.identity, new Vector3(ratioX, ratioY, 1));
+        if (UI.isOn == true)
+        {
+            resultScript.ThreeResults("Height(m)", "Time(s)", "Gravity(m/s^2)", heightResults, time1Results, gResults, i);
+        }
     }
 }
